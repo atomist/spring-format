1aa86f647c9894eb4b672d6f987432ee66f010de
lievendoclo <lieven@atomist.com>
2018-10-30 14:22:13 +0100

    Cleanup

--- src/main/java/com/atomist/Formatter.java
+++ src/main/java/com/atomist/Formatter.java
@@ -1,29 +1,34 @@
 
 package com.atomist;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 import io.spring.javaformat.formatter.FileEdit;
 import io.spring.javaformat.formatter.FileFormatter;
 import io.spring.javaformat.formatter.FileFormatterException;
 
 public class Formatter {
-
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 		String path = System.getProperty("user.dir");
 		System.out.println("Formatting Java files under " + path);
 		try {
 			FileFormatter formatter = new FileFormatter();
-			Files.walk(Paths.get(path)).filter(Files::isRegularFile)
-					.filter(f -> f.getFileName().toString().endsWith("java"))
-					.map(f -> formatter.formatFile(f.toFile(), StandardCharsets.UTF_8))
-					.forEach(Formatter::save);
-		}
-		catch (FileFormatterException ex) {
-			throw new Exception("Unable to format file " + ex.getFile(), ex);
+			try(Stream<Path> s = Files.walk(Paths.get(path))){
+				s.filter(Files::isRegularFile)
+							.filter(f -> f.getFileName().toString().endsWith("java"))
+							.map(f -> formatter.formatFile(f.toFile(), StandardCharsets.UTF_8))
+							.forEach(Formatter::save);
+			}
+		} catch (FileFormatterException ex) {
+			throw new RuntimeException("Unable to format file " + ex.getFile(), ex);
+		} catch (IOException ioEx) {
+			throw new RuntimeException("Filesystem error", ioEx);
 		}
 	}
 
