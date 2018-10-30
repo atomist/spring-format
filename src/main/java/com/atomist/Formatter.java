d4fb27b8dc42927d1472057085407901ad66dcc6
Rod Johnson <rod@atomist.com>
2018-10-30 20:59:24 +1100

    build Uberjar

--- src/main/java/com/atomist/Formatter.java
+++ src/main/java/com/atomist/Formatter.java
@@ -12,24 +12,24 @@ import io.spring.javaformat.formatter.FileFormatterException;
 
 public class Formatter {
 
-    public static void main(String args[]) throws Exception {
-        String path = args[0];
-        try {
-            FileFormatter formatter = new FileFormatter();
-            Files.walk(Paths.get(path))
-                .filter(Files::isRegularFile)
-                .filter(f -> f.getFileName().toString().endsWith("java"))
-                .map(f -> formatter.formatFile(f.toFile(), StandardCharsets.UTF_8))
-                .forEach(Formatter::save);
-        }
-        catch (FileFormatterException ex) {
-            throw new Exception("Unable to format file " + ex.getFile(), ex);
-        }
-    }
+	public static void main(String args[]) throws Exception {
+		String path = System.getProperty("user.dir");
+		System.out.println("Formatting Java files under " + path);
+		try {
+			FileFormatter formatter = new FileFormatter();
+			Files.walk(Paths.get(path)).filter(Files::isRegularFile)
+					.filter(f -> f.getFileName().toString().endsWith("java"))
+					.map(f -> formatter.formatFile(f.toFile(), StandardCharsets.UTF_8))
+					.forEach(Formatter::save);
+		}
+		catch (FileFormatterException ex) {
+			throw new Exception("Unable to format file " + ex.getFile(), ex);
+		}
+	}
 
-    private static void save(FileEdit edit) {
+	private static void save(FileEdit edit) {
 		System.out.println("Formatting file " + edit.getFile());
 		edit.save();
 	}
-    
+
 }
